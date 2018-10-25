f79dd1ae0a8d95c1456a583ab603a4ab3722d258
Aliffiro <fathf48@gmail.com>
2018-10-25 09:15:54 +0700

    Init Upload
    Dari Alip

---
+++ login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP-SQL Login</title>
+</head>
+<body>
+<h1>Login</h1>
+<form method="post" action="login.php">
+	<input type="text" name = "username" placeholder="Enter username">
+	<input type="password" name="password" placeholder="Enter password here">
+	<input type="submit" name="submit" value="Login">
+</form>
+
+<a href="register.php" >Register</a>
+
+</body>
+</html>
+
+<?php
+session_start();
+
+if($_POST['submit']) {
+	include_once('connection.php');
+	$username = strip_tags($_POST['username']);
+	$password = strip_tags($_POST['password']);
+
+	$sql = "SELECT id,username,password FROM user where username = '$username' LIMIT 1";
+	$query = mysqli_query($db, $sql);
+	if($query) {
+		$row = mysqli_fetch_row($query);
+		$userId= $row[0];
+		$dbUserName = $row[1];
+		$dbPassword = $row[2];
+	}
+	if($username == $dbUserName && $password == $dbPassword) {
+		$_SESSION['username'] = $username;
+		$_SESSION['id'] = $userId;
+		header('Location: index.php');
+	}
+	else {
+		echo "<b><i>Incorrect credentials</i><b>";
+
+	}
+}
+
+?>
